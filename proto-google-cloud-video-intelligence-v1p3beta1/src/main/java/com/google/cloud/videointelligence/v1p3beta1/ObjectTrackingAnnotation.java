2a3a6e8c26d4cb08162b81d04c2851419f483498
yoshi-automation <yoshi-automation@google.com>
2020-03-06 13:23:35 -0800

    Changes triggered by https://github.com/googleapis/googleapis.git: commit 91e1fb5ef9829c0c7a64bfa5bde330e6ed594378 Author: Google APIs <noreply@google.com> Date:   Fri Mar 6 11:53:17 2020 -0800
        chore: update protobuf (protoc) version to 3.11.2
    
        PiperOrigin-RevId: 299404145

--- proto-google-cloud-video-intelligence-v1p3beta1/src/main/java/com/google/cloud/videointelligence/v1p3beta1/ObjectTrackingAnnotation.java
+++ proto-google-cloud-video-intelligence-v1p3beta1/src/main/java/com/google/cloud/videointelligence/v1p3beta1/ObjectTrackingAnnotation.java
@@ -173,7 +173,10 @@ public final class ObjectTrackingAnnotation extends com.google.protobuf.Generate
   private int trackInfoCase_ = 0;
   private java.lang.Object trackInfo_;
 
-  public enum TrackInfoCase implements com.google.protobuf.Internal.EnumLite {
+  public enum TrackInfoCase
+      implements
+          com.google.protobuf.Internal.EnumLite,
+          com.google.protobuf.AbstractMessage.InternalOneOfEnum {
     SEGMENT(3),
     TRACK_ID(5),
     TRACKINFO_NOT_SET(0);
